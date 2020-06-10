212331f7b530b8de1afb91d6caeecb01f338e8ca
Vitali Kirychenka <lisius@gmail.com>
2020-06-10 17:33:36 +0300

    Updated functional test

--- tests/Feature/App/Console/Commands/ParseLogTest.php
+++ tests/Feature/App/Console/Commands/ParseLogTest.php
@@ -11,11 +11,35 @@ class ParseLogTest extends TestCase
     /**
      * @return void
      */
-    public function testExample()
+    public function testSuccess()
     {
-        //No easy way to test multiline output
-        //https://github.com/laravel/framework/pull/32927
-        $this->artisan('parse:log', ['./tests/Feature/App/Console/Commands/test.log'])
-            ->assertExitCode(0);
+        $expected = <<<OUT
++-----+--------+
+| URI | Visits |
++-----+--------+
+| /1  | 2      |
+| /2  | 1      |
++-----+--------+
++-----+---------------+
+| URI | Unique Visits |
++-----+---------------+
+| /1  | 1             |
+| /2  | 1             |
++-----+---------------+
+OUT;
+        $command = $this->artisan('parse:log', ['logfile' => './tests/Feature/App/Console/Commands/test.log']);
+        $expected = explode(PHP_EOL, $expected);
+        foreach ($expected as $line) {
+            $command->expectsOutput($line);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function testFileNotFound()
+    {
+        $this->artisan('parse:log', ['logfile' => 'non_existent_file'])
+            ->expectsOutput('Error: fopen(non_existent_file): failed to open stream: No such file or directory');
     }
 }
